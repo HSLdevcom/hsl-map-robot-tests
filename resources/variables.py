5f565c2d792a4a1203133cf23f9869b41903fe63
Tommi Lilja <tommi.lilja@twoday.com>
2026-02-11 09:47:14 +0200

    Added simple stop poster comparison test

--- resources/variables.py
+++ resources/variables.py
@@ -5,7 +5,8 @@ load_dotenv(".env")
 HEADLESS = os.getenv("HEADLESS", "true")
 
 EXAMPLE_URL = os.getenv("EXAMPLE_URL", "https://example.com")
-HSL_MAP_PUBLISHER_URL = os.getenv("HSL_MAP_PUBLISHER_URL", "https://dev.kartat.hsl.fi/julkaisin/")
+HSL_MAP_PUBLISHER_DEV_URL = os.getenv("HSL_MAP_PUBLISHER_DEV_URL", "https://dev.kartat.hsl.fi/julkaisin/")
+HSL_MAP_PUBLISHER_PROD_URL = os.getenv("HSL_MAP_PUBLISHER_PROD_URL", "https://kartat.hsl.fi/julkaisin/")
 
 HSL_MAP_PUBLISHER_USER = os.getenv("HSL_MAP_PUBLISHER_USER", "")
 HSL_MAP_PUBLISHER_PASSWORD = os.getenv("HSL_MAP_PUBLISHER_PASSWORD", "")
